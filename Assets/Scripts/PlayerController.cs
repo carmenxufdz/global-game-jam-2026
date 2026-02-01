5ee585a99e65f559ae47f2436733221c961024c2
AntonioFdez03 <antonio.fers2003@gmail.com>
2026-02-01 11:03:23 +0100

    sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -139,7 +139,7 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(collision.gameObject);
             maskManager.gotMask = true;
-            NotificationManager.Instance.Show("Press E to wear the mask");
+            //NotificationManager.Instance.Show("Press E to wear the mask");
         }
     }
 

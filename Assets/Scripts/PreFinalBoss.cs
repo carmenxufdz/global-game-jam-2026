ad21ecc0dab77e7097f17a10d8dd35f3be6ccd01
AntonioFdez03 <antonio.fers2003@gmail.com>
2026-02-01 13:19:53 +0100

    cambios de mascara al final funcionales

--- Assets/Scripts/PreFinalBoss.cs
+++ Assets/Scripts/PreFinalBoss.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PreFinalBoss : MonoBehaviour
-{
-    
+{   
+    [SerializeField] MaskManager maskManager;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            maskManager.mask = true;
+            maskManager.ActualizarMundo();   
+        }
+    }
 }

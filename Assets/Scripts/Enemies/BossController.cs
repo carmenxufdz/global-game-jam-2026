0c00dc0ac308bdbee263166743e380cc21ff8c8e
AntonioFdez03 <antonio.fers2003@gmail.com>
2026-02-01 12:27:09 +0100

    boss

--- Assets/Scripts/Enemies/BossController.cs
+++ Assets/Scripts/Enemies/BossController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
 
-    [SerializeField] int maxHealth;
-    [SerializeField] int currentHealth;
+    private int maxHealth;
+    private int currentHealth;
+    private 
     Rigidbody2D rb;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         maxHealth = 100;
         currentHealth = maxHealth;
@@ -19,7 +23,10 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentHealth <= 0)
+        {
+            animator.SetBool("Dead",true);
+        }
     }
 
     public int GetCurrentHealth() => currentHealth;
